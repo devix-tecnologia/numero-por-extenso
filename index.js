0e718587304f707be9a9011707452122a355bc85
scart97 <scart.lucas@gmail.com>
2024-02-19 15:01:07 -0300

    feat: Convertendo cjs para esm
    BREAKING CHANGE:

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const estilos = {
-    normal: 'normal',
-    monetario: 'monetario',
-    porcentagem: 'porcentagem'
-};
+import monetario from "./lib/estilos/monetario";
+import normal from "./lib/estilos/normal";
+import porcentagem from "./lib/estilos/porcentagem";
 
-module.exports = {
-    porExtenso,
-    estilo: estilos
-};
-
-function porExtenso(numero, estilo = estilos.normal) {
-    if (!estilos[estilo])
-        throw new Error(`Estilo "${estilo}" inválido. Possíveis valores: normal, monetario, porcentagem`);
-
-    return require('./lib/estilos/' + estilo)(numero);
-}
-
+export { monetario, normal, porcentagem };
